412c378ac98b3b587560ffdcd0708ecd788df14e
Paulo Marcelo <marcelomp97447@gmail.com>
2025-04-17 18:59:11 -0300

    introduzindo o bootstrap e adicionando o header e footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <footer class="bg-danger text-white text-center py-3">
+    <p>&copy; {{ new Date().getFullYear() }} Pokedex. All rights reserved.</p>
+    <p>Made with ❤️ by Paulo Marcelo</p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 90px;
+  left: 0;
+  z-index: 1;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/">Pokedex</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <router-link class="nav-link" aria-current="page" to="/"
+            >Home</router-link
+          >
+          <router-link class="nav-link" to="/about">About</router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup></script>
